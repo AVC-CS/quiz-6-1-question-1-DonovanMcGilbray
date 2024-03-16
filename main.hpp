81a640bf89cdb7a8e0d6050d1c18c7f289648abb
DonovanMcGilbray <156950567+DonovanMcGilbray@users.noreply.github.com>
2024-03-15 23:09:31 -0700

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,8 +1,39 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &begin, int &end) {
+  cout << "Enter two integer values (begin < end): ";
+  cin >> begin >> end;
+  while(begin >= end) {
+    cout << "Begin must be less than end\n";
+    cout << "Enter two integer values (begin < end): ";
+    cin >> begin >> end;
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+int isPrime(int num) {
+  if(num <= 3) {
+    return true;
+  }
+  for(int i = 4; i * i <= num; i++) {
+    if(num % i == 0) {
+      return false;
+    }
+  return true;
+  }
+}
+
+int getNextPrime(int begin) {
+  int nextPrime = begin + 1;
+  while(!isPrime(nextPrime)) {
+    nextPrime++;
+  }
+  return nextPrime;
+}
+
+int getPrevPrime(int end) {
+  int prevPrime = end - 1;
+  while(!isPrime(prevPrime)) {
+    prevPrime--;
+  }
+  return prevPrime;
+}
